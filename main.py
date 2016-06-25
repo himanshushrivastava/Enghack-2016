870d62ab853b4c41dc65658bcabdcbfbdd40f7c4
Himanshu Shrivastava <himanshu.shrivastava12@gmail.com>
2016-06-25 17:16:03 -0400

    Added capturing of each card under each list

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ token_url = trello.get_token_url('Trello Application', expires='30days', write_a
 print 'Navigate to the following webpage and click allow to recieve your Trello token'
 print token_url
 user_token = raw_input('Enter your token: ')
-
 trello.set_token(user_token)
+
 board_id = '576ec71a66056953359c2bcd'
 
-board_information = trello.boards.get(board_id)
-cards = trello.boards.get_card(board_id)
-card_id_list = []
+board = trello.boards.get(board_id)
+card_lists = trello.boards.get_list(board_id)
 
-for card in cards:
-    for key, value in card.items():
-        print key, ": ", value
-    card_id_list.append(card['id'])
-
-for card_id in card_id_list:
-    print trello.cards.get_field('name', card_id)
+for card_list in card_lists:
+    # for key, value in card_list.items():
+    #     print key, ": ", value
+    print ""
+    print card_list['name']
+    print ""
+    cards = trello.lists.get_card(card_list['id'])
+    for card in cards:
+        card_values = trello.cards.get_field('name', card['id'])
+        print card_values['_value']
