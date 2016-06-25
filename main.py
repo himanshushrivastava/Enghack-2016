79afc44f2a11848ee8d6c8300942723a4dc6f2d8
Himanshu Shrivastava <himanshu.shrivastava12@gmail.com>
2016-06-25 15:36:24 -0400

    Added authentication code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print "Hello world"
+from trello import TrelloApi
+
+trello = TrelloApi('a4ae903d87894a87ba4c6a7b7bf617bd')
+token_url = trello.get_token_url('Trello Application', expires='30days', write_access=True)
+
+print 'Navigate to the following webpage and click allow to recieve your Trello token'
+print token_url
+user_token = raw_input('Enter your token: ')
+
+trello.set_token(user_token)
